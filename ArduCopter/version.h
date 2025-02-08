f9230f01f6cf9725b7c064f9a4a3d0a8ad23ba73
Luca Pescante <luca@pescante.it>
2025-02-08 15:43:13 +0800

    version to 3.0.0-beta1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.5.7, Align V3.0.0-beta"
+#define THISFIRMWARE "ArduCopter V4.5.7, Align V3.0.0-beta1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_OFFICIAL

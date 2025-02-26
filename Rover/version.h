4f9c7db72534f7b2c1064de6c332cc7154e5ba6e
Luca Pescante <luca@pescante.it>
2025-02-26 17:13:45 +0800

    Rover: version to 3.0.0-beta

--- Rover/version.h
+++ Rover/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.5.7"
+#define THISFIRMWARE "ArduRover V4.5.7, Align V3.0.0-beta"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_OFFICIAL
@@ -18,9 +18,9 @@
 #define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
 
 // Align version 
-#define MIDDLE_MAJOR 2
+#define MIDDLE_MAJOR 3
 #define MIDDLE_MINOR 0
 #define MIDDLE_PATCH 0
-#define MIDDLE_TYPE FIRMWARE_VERSION_TYPE_DEV
+#define MIDDLE_TYPE FIRMWARE_VERSION_TYPE_BETA
 
 #include <AP_Common/AP_FWVersionDefine.h>

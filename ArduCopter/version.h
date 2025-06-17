7becf59d3a04c342f30ef27816edfb9d024f99a7
Luca Pescante <luca@pescante.it>
2025-06-17 12:15:24 +0200

    ArduCopter: version to 3.2.3

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.5.7 Align V3.2.2"
+#define THISFIRMWARE "ArduCopter V4.5.7 Align V3.2.3"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_OFFICIAL
@@ -20,7 +20,7 @@
 // Align version 
 #define MIDDLE_MAJOR 3
 #define MIDDLE_MINOR 2
-#define MIDDLE_PATCH 2
+#define MIDDLE_PATCH 3
 #define MIDDLE_TYPE FIRMWARE_VERSION_TYPE_DEV
 
 #include <AP_Common/AP_FWVersionDefine.h>

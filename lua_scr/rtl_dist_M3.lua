4a1bbaecdeecb7a10131d6a53d72d57f5f6838fd
Luca Pescante <luca@pescante.it>
2025-11-17 16:15:02 +0800

    lua_scr/rtl_dist_M3: use RTLS_ENABLE to use different batteries

--- lua_scr/rtl_dist_M3.lua
+++ lua_scr/rtl_dist_M3.lua
@@ -4,22 +4,27 @@
 --- Configuration parameters
 ----------------------------------------------
 local BATTERY_CELLS = 6
+
+-- {cell voltage, M3 4200 mAh, M3 4800 mAh HV}
 local VOLTAGE_TO_PERCENT_TABLE = {
-    {3.4, 0},
-    {3.5, 4},
-    {3.6, 11},
-    {3.7, 42},
-    {3.8, 65},
-    {3.9, 79},
-    {4.0, 90},
-    {4.1, 100}
+    {3.4,   0,   0},
+    {3.5,   4,   4},
+    {3.6,  11,  12},
+    {3.7,  42,  38},
+    {3.8,  65,  56},
+    {3.9,  79,  69},
+    {4.0,  90,  79},
+    {4.1, 100,  89},
+    {4.2, 100,  98},
+    {4.3, 100, 100},
 }
 
 -- %/m = 1/(SPEED[m/s]*36*BATT_CAPACITY[Ah]/CRUISE_CURRENT[A])
-local PERCENT_PER_METER = 0.012 -- Battery percentage consumed per meter of travel
+local PERCENT_PER_METER = 0.012  -- Battery percentage consumed per meter of travel
 local PERCENT_PER_METER_DOWN = 0.03  -- Battery percentage consumed per meter of travel
-local MIN_SAFE_PERCENT = 7    -- Minimum battery percentage needed to return home
+local MIN_SAFE_PERCENT = 7  -- Minimum battery percentage needed to return home
 local MINIMUM_RTL_DIST = 30  -- Minimum distance from home to trigger RTL
+local DEFAULT_BATTERY_ID = 2  -- 4800 mAh HV as default
 
 ----------------------------------------------
 --- script start, don't change below this line
@@ -28,6 +33,7 @@ local UPDATE_VOLTAGE_MS = 200
 local VOLTAGES_SAMPLES_TO_MEDIAN = 51
 local RTL_MODE = 6  -- Mode number for RTL
 local PARAM_TABLE_KEY = 82
+local NUM_COLUMNS = #VOLTAGE_TO_PERCENT_TABLE[1]
 
 -- Variables
 local voltage_samples = {}
@@ -44,13 +50,18 @@ end
 -- add param table
 assert(param:add_table(PARAM_TABLE_KEY, "RTLS_", 2), 'rtl_dist: could not add param table')
 
-local RTLS_ENABLE = bind_add_param('ENABLE', 1, 1)
+local RTLS_ENABLE = bind_add_param('ENABLE', 1, DEFAULT_BATTERY_ID)
 local RTLS_DIST = bind_add_param('DIST', 2, MINIMUM_RTL_DIST)
 
 function update()
     -- return if not enabled
     if RTLS_ENABLE:get() < 1 then
         return update, 1000
+    end
+    if  RTLS_ENABLE:get() > NUM_COLUMNS - 1 then
+        -- report error and delay script
+        gcs:send_text(0, "rtl_dist: battery id out of range")
+        return update, 30000
     end
 
     -- return if not armed
@@ -83,6 +94,11 @@ function update()
 
     -- Convert voltage to percentage using lookup table
     local percent = voltage_to_percent(median_voltage)
+    if percent < 0 then
+        -- report error and delay script
+        gcs:send_text(0, "rtl_dist: error calculating percentage")
+        return update, 10000
+    end
 
     -- Get distance from home and altitude
     local home = ahrs:get_home()
@@ -118,22 +134,25 @@ end
 
 -- Function to convert voltage to percentage using lookup table
 function voltage_to_percent(voltage)
+    -- add 1 so you get the table column (starts from 2)
+    local batt_id = RTLS_ENABLE:get() + 1
+
     -- divide for number of cells
     voltage = voltage / BATTERY_CELLS
 
     -- Handle edge cases
     if voltage <= VOLTAGE_TO_PERCENT_TABLE[1][1] then
-        return VOLTAGE_TO_PERCENT_TABLE[1][2]
+        return VOLTAGE_TO_PERCENT_TABLE[1][batt_id]
     elseif voltage >= VOLTAGE_TO_PERCENT_TABLE[#VOLTAGE_TO_PERCENT_TABLE][1] then
-        return VOLTAGE_TO_PERCENT_TABLE[#VOLTAGE_TO_PERCENT_TABLE][2]
+        return VOLTAGE_TO_PERCENT_TABLE[#VOLTAGE_TO_PERCENT_TABLE][batt_id]
     end
 
     -- Find the two closest voltage entries in the table
     for i = 1, #VOLTAGE_TO_PERCENT_TABLE - 1 do
         local v1 = VOLTAGE_TO_PERCENT_TABLE[i][1]
-        local p1 = VOLTAGE_TO_PERCENT_TABLE[i][2]
+        local p1 = VOLTAGE_TO_PERCENT_TABLE[i][batt_id]
         local v2 = VOLTAGE_TO_PERCENT_TABLE[i+1][1]
-        local p2 = VOLTAGE_TO_PERCENT_TABLE[i+1][2]
+        local p2 = VOLTAGE_TO_PERCENT_TABLE[i+1][batt_id]
 
         if voltage >= v1 and voltage <= v2 then
             -- Linear interpolation
@@ -141,7 +160,8 @@ function voltage_to_percent(voltage)
         end
     end
 
-    return 0  -- Fallback
+    -- should be impossible to get here
+    return -1
 end
 
 gcs:send_text(6, "rtl_dist.lua is running")

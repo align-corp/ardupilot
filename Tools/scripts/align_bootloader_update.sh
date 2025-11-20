9387b015459030e4e9245dc7a7c1a0aabc1f37c8
Luca Pescante <luca@pescante.it>
2025-11-20 16:52:18 +0800

    Tools/scripts/align_bootloader_update: add MR25

--- Tools/scripts/align_bootloader_update.sh
+++ Tools/scripts/align_bootloader_update.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 # Generate bootloaders
+#TODO: horrible, use loop
 
 # Align AP6m
-src="build/AP6m/bin/AP_Bootloader.bin"
 ./waf configure --board AP6m --bootloader
 ./waf bootloader
-if [ -f $src ]; then
-    cp $src "Tools/bootloaders/AP6m_bl.bin"
-    cp $src "Tools/bootloaders/AP6m-M3_bl.bin"
-    cp $src "Tools/bootloaders/AP6m-M3-OF_bl.bin"
-    cp $src "Tools/bootloaders/AP6m-M3-A10_bl.bin"
-    echo "Update bootloader for AP6m"
-else
-    echo "Bootloader doesn't exist"
-    exit 2
-fi
+src="build/AP6m/bin/AP_Bootloader.bin"
+./waf configure --board AP6m-MR25 --bootloader
+./waf bootloader
+src2="build/AP6m-MR25/bin/AP_Bootloader.bin"
+
+# copy
+cp $src "Tools/bootloaders/AP6m_bl.bin"
+cp $src "Tools/bootloaders/AP6m-M3_bl.bin"
+cp $src "Tools/bootloaders/AP6m-M3-OF_bl.bin"
+cp $src "Tools/bootloaders/AP6m-M3-A10_bl.bin"
+cp $src2 "Tools/bootloaders/AP6m-MR25_bl.bin"
+cp $src2 "Tools/bootloaders/AP6m-MR25-A10_bl.bin"
+cp $src2 "Tools/bootloaders/AP6m-MR25-no-gps_bl.bin"
+echo "Update bootloader for AP6m"

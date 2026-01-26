1222848962976a2d6b8452251ce375b13d67fae3
Luca Pescante <luca@pescante.it>
2026-01-26 11:14:28 +0800

    Tools: MR25 uses the same bootloader as M3

--- Tools/scripts/align_bootloader_update.sh
+++ Tools/scripts/align_bootloader_update.sh
@@ -7,16 +7,13 @@ set -e
 ./waf configure --board AP6m --bootloader
 ./waf bootloader
 src="build/AP6m/bin/AP_Bootloader.bin"
-./waf configure --board AP6m-MR25 --bootloader
-./waf bootloader
-src2="build/AP6m-MR25/bin/AP_Bootloader.bin"
 
 # copy
 cp $src "Tools/bootloaders/AP6m_bl.bin"
 cp $src "Tools/bootloaders/AP6m-M3_bl.bin"
 cp $src "Tools/bootloaders/AP6m-M3-OF_bl.bin"
 cp $src "Tools/bootloaders/AP6m-M3-A10_bl.bin"
-cp $src2 "Tools/bootloaders/AP6m-MR25_bl.bin"
-cp $src2 "Tools/bootloaders/AP6m-MR25-A10_bl.bin"
-cp $src2 "Tools/bootloaders/AP6m-MR25-no-gps_bl.bin"
-echo "Update bootloader for AP6m"
+cp $src "Tools/bootloaders/AP6m-MR25_bl.bin"
+cp $src "Tools/bootloaders/AP6m-MR25-A10_bl.bin"
+cp $src "Tools/bootloaders/AP6m-MR25-no-gps_bl.bin"
+echo "AP6m bootloaders updated"

8fd2c5b06d9ce95e3368679e19aa4159db5e61ad
Luca Pescante <luca@pescante.it>
2026-03-28 17:12:04 +0800

    lua_src/led: add DroneCAN

--- lua_scr/led.lua
+++ lua_scr/led.lua
@@ -1,17 +1,21 @@
 -- control serial LEDs for navigation lights - unified version for M3 and M460
 -- set parameter SERVOx_FUNCTION to 94 (Script1) to control LEDs
 -- the SERVO output must be a GPIO MCU pin (9-14 on PixHawk)
+-- support CAN: DroneCAN LED notify driver must be disabled, to prevent conflicts
 
 -- enable parameters
 local PARAM_TABLE_KEY = 46
 local PARAM_TABLE_PREFIX = "LED_"
 assert(param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 2), 'could not add param table')
-assert(param:add_param(PARAM_TABLE_KEY, 1, 'ENABLE', 1), 'could not add ENABLE param')
+assert(param:add_param(PARAM_TABLE_KEY, 1, 'ENABLE', 1), 'could not add LED_ENABLE param')
+assert(param:add_param(PARAM_TABLE_KEY, 2, 'COLOR', 0), 'could not add LED_COLOR param')
 local led_enable = Parameter("LED_ENABLE")
+local led_color = Parameter("LED_COLOR")
 
--- align_vehicles
+-- vehicle types
 local M3 = 0
 local M4 = 1
+local M4_CAN = 2
 
 -- flight mode constants
 local LOITER_MODE = 5
@@ -30,18 +34,47 @@ local WHITE_LOW = 3
 local WHITE_HIGH = 4
 local ORANGE = 5
 
--- global variables
+local color_values = {
+    [RED]        = {255,   0,   0},
+    [GREEN]      = {  0, 255,   0},
+    [BLUE]       = {  0,   0, 255},
+    [WHITE_LOW]  = { 85,  85,  85},
+    [WHITE_HIGH] = {255, 255, 255},
+    [ORANGE]     = {175,  80,   0}
+}
+
+-- global state
 local count = 0
 local channels = {}
 local num_leds_per_channel
 local align_vehicle
+
+-- intermediate LED state: position -> {r, g, b}
+local led_colors = {
+    front_right = {0, 0, 0},
+    front_left  = {0, 0, 0},
+    rear_left   = {0, 0, 0},
+    rear_right  = {0, 0, 0}
+}
+
+-- last sent LED state; initialised to -1 to ensure first frame is always sent
+local prev_led_colors = {
+    front_right = {-1, -1, -1},
+    front_left  = {-1, -1, -1},
+    rear_left   = {-1, -1, -1},
+    rear_right  = {-1, -1, -1}
+}
 
 -- configuration based on vehicle type
 local function setup_channels()
     local align_frame = FWVersion:os_sw_version():toint()
     if align_frame > 0x03010000 and align_frame < 0x03070000 then
         -- AP6-M450 AP6-M460 AP6-M490
-        align_vehicle = M4
+        if align_frame & 0xF >= 9 then
+            align_vehicle = M4_CAN
+        else
+            align_vehicle = M4
+        end
     elseif align_frame > 0x04070000 and align_frame < 0x04080000 then
         -- AP6m-M3
         align_vehicle = M3
@@ -55,9 +88,9 @@ local function setup_channels()
         end
         channels = {
             front_right = {chan = chan + 1, led = 0},
-            front_left = {chan = chan + 1, led = 1},
-            rear_left = {chan = chan + 1, led = 2},
-            rear_right = {chan = chan + 1, led = 3}
+            front_left  = {chan = chan + 1, led = 1},
+            rear_left   = {chan = chan + 1, led = 2},
+            rear_right  = {chan = chan + 1, led = 3}
         }
         num_leds_per_channel = 4
         serialLED:set_num_neopixel(chan + 1, num_leds_per_channel)
@@ -69,16 +102,15 @@ local function setup_channels()
         local chan2 = SRV_Channels:find_channel(95)
         local chan3 = SRV_Channels:find_channel(96)
         local chan4 = SRV_Channels:find_channel(97)
-
         if not chan1 or not chan2 or not chan3 or not chan4 then
             gcs:send_text(6, "LEDs: M460 channels not set")
             return false
         end
         channels = {
-            front_right = {chan = chan2 + 1, led = nil},
-            front_left = {chan = chan4 + 1, led = nil},
-            rear_left = {chan = chan3 + 1, led = nil},
-            rear_right = {chan = chan1 + 1, led = nil}
+            front_right = {chan = chan2 + 1},
+            front_left  = {chan = chan4 + 1},
+            rear_left   = {chan = chan3 + 1},
+            rear_right  = {chan = chan1 + 1}
         }
         num_leds_per_channel = 3
         serialLED:set_num_neopixel(chan1 + 1, num_leds_per_channel)
@@ -87,82 +119,93 @@ local function setup_channels()
         serialLED:set_num_neopixel(chan4 + 1, num_leds_per_channel)
         gcs:send_text(6, "LED: M460")
         return true
+
+    elseif align_vehicle == M4_CAN then
+        -- light_id = motor index: 0=front_right, 1=rear_left, 2=front_left, 3=rear_right
+        channels = {
+            front_right = {light_id = 0},
+            rear_left   = {light_id = 1},
+            front_left  = {light_id = 2},
+            rear_right  = {light_id = 3}
+        }
+        gcs:send_text(6, "LED: M4_CAN")
+        return true
     end
+
     gcs:send_text(5, "LED: unknown vehicle, stopping")
     return false
 end
 
--- color setting functions
-local color_values = {
-    [RED] = {255, 0, 0},
-    [GREEN] = {0, 255, 0},
-    [BLUE] = {0, 0, 255},
-    [WHITE_LOW] = {85, 85, 85},
-    [WHITE_HIGH] = {255, 255, 255},
-    [ORANGE] = {175, 80, 0}
-}
-
-local function set_channel_color(chan, led_index, color)
-    local rgb = color_values[color]
-    if not rgb then return end
-
-    if align_vehicle == M3 then
-        serialLED:set_RGB(chan, led_index, rgb[1], rgb[2], rgb[3])
-    elseif align_vehicle == M4 then
-        for i = 0, num_leds_per_channel - 1 do
-            serialLED:set_RGB(chan, i, rgb[1], rgb[2], rgb[3])
-        end
-    end
-end
-
-local function clear_channel(chan, led_index)
-    if align_vehicle == M3 then
-        serialLED:set_RGB(chan, led_index, 0, 0, 0)
-    elseif align_vehicle == M4 then
-        for i = 0, num_leds_per_channel - 1 do
-            serialLED:set_RGB(chan, i, 0, 0, 0)
-        end
-    end
-end
-
--- set LED color by position
+-- set LED color by position (writes to intermediate state only)
 local function set_led_color(position, color)
-    local ch_info = channels[position]
-    if ch_info then
-        set_channel_color(ch_info.chan, ch_info.led, color)
+    local rgb = color_values[color]
+    if rgb then
+        led_colors[position] = rgb
     end
 end
 
 -- set all LEDs to specified colors
 local function set_all_leds(front_color, rear_color)
-    set_led_color("front_left", front_color)
+    set_led_color("front_left",  front_color)
     set_led_color("front_right", front_color)
-    set_led_color("rear_left", rear_color)
-    set_led_color("rear_right", rear_color)
+    set_led_color("rear_left",   rear_color)
+    set_led_color("rear_right",  rear_color)
 end
 
 -- turn all LEDs off
 local function leds_off()
-    for _, ch_info in pairs(channels) do
-        clear_channel(ch_info.chan, ch_info.led)
+    for pos in pairs(led_colors) do
+        led_colors[pos] = {0, 0, 0}
     end
 end
 
--- send updates to all channels
+-- send led_colors to the appropriate hardware driver (skips send if nothing changed)
 local function send_led_updates()
+    local changed = false
+    for pos in pairs(led_colors) do
+        local c, p = led_colors[pos], prev_led_colors[pos]
+        if c[1] ~= p[1] or c[2] ~= p[2] or c[3] ~= p[3] then
+            changed = true
+            break
+        end
+    end
+    if not changed then return end
+    for pos in pairs(led_colors) do
+        local c = led_colors[pos]
+        prev_led_colors[pos] = {c[1], c[2], c[3]}
+    end
+
     if align_vehicle == M3 then
-        local ch_info = channels.front_right -- any channel will work since they're all the same
-        serialLED:send(ch_info.chan)
+        -- all positions share one chain; set each LED index then send once
+        for pos, ch_info in pairs(channels) do
+            local rgb = led_colors[pos]
+            serialLED:set_RGB(ch_info.chan, ch_info.led, rgb[1], rgb[2], rgb[3])
+        end
+        serialLED:send(channels.front_right.chan)
+
     elseif align_vehicle == M4 then
+        -- stage all RGB values first, then send all channels
+        for pos, ch_info in pairs(channels) do
+            local rgb = led_colors[pos]
+            for i = 0, num_leds_per_channel - 1 do
+                serialLED:set_RGB(ch_info.chan, i, rgb[1], rgb[2], rgb[3])
+            end
+        end
         for _, ch_info in pairs(channels) do
             serialLED:send(ch_info.chan)
         end
+
+    elseif align_vehicle == M4_CAN then
+        for pos, ch_info in pairs(channels) do
+            local rgb = led_colors[pos]
+            droneCAN:set_led(ch_info.light_id, rgb[1], rgb[2], rgb[3])
+        end
+        droneCAN:send_leds()
     end
 end
 
 -- main LED control function
 function led()
-    -- check if LEDs are enabled
     if led_enable:get() < 1 or notify:is_powering_off() then
         leds_off()
         send_led_updates()
@@ -170,8 +213,12 @@ function led()
     end
 
     local mode = vehicle:get_mode()
+    local front_color = WHITE_LOW
+    if led_color:get() == 1 then
+        -- use red as front color
+        front_color = RED
+    end
 
-    -- determine LED pattern based on flight mode
     if not arming:is_armed() and not arming:pre_arm_checks() then
         set_all_leds(RED, RED)
     elseif mode == ALTHOLD_MODE then
@@ -179,42 +226,41 @@ function led()
         if count == 0 then
             set_all_leds(WHITE_HIGH, WHITE_HIGH)
         else
-            set_all_leds(WHITE_LOW, GREEN)
+            set_all_leds(front_color, GREEN)
         end
     elseif mode == LOITER_MODE then
         -- 2 white blinks on green background
         if count == 0 or count == 2 then
             set_all_leds(WHITE_HIGH, WHITE_HIGH)
         else
-            set_all_leds(WHITE_LOW, GREEN)
+            set_all_leds(front_color, GREEN)
         end
     elseif mode == AUTO_MODE or mode == GUIDED_MODE or mode == FOLLOW_MODE then
         -- 2 white blinks on blue background
         if count == 0 or count == 2 then
             set_all_leds(WHITE_HIGH, WHITE_HIGH)
         else
-            set_all_leds(WHITE_LOW, BLUE)
+            set_all_leds(front_color, BLUE)
         end
     elseif mode == RTL_MODE then
         -- 2 white blinks on orange background
         if count == 0 or count == 2 then
             set_all_leds(WHITE_HIGH, WHITE_HIGH)
         else
-            set_all_leds(WHITE_LOW, ORANGE)
+            set_all_leds(front_color, ORANGE)
         end
     elseif mode == LAND then
         -- 3 white blinks on orange background
         if count == 0 or count == 2 or count == 4 then
             set_all_leds(WHITE_HIGH, WHITE_HIGH)
         else
-            set_all_leds(WHITE_LOW, ORANGE)
+            set_all_leds(front_color, ORANGE)
         end
     else
         -- unknown mode: orange lights
         set_all_leds(ORANGE, ORANGE)
     end
 
-    -- update count for blinking pattern
     count = count + 1
     if count > 9 then
         count = 0

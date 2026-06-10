d6a00268f56773f53ad423aadf5530f19467edab
Luca Pescante <luca@pescante.it>
2026-06-10 17:07:33 +0800

    ArduCopter/mode_guided: relaxy use rngfnd for takeoff logic

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -124,7 +124,7 @@ bool ModeGuided::do_user_takeoff_start(float takeoff_alt_cm)
     bool alt_target_terrain = false;
     if (wp_nav->rangefinder_used_and_healthy() &&
         wp_nav->get_terrain_source() == AC_WPNav::TerrainSource::TERRAIN_FROM_RANGEFINDER &&
-        takeoff_alt_cm < copter.rangefinder.max_distance_cm_orient(ROTATION_PITCH_270)) {
+        takeoff_alt_cm < (copter.rangefinder.max_distance_cm_orient(ROTATION_PITCH_270) * 0.9f - 100)) {
         // can't takeoff downwards
         if (takeoff_alt_cm <= copter.rangefinder_state.alt_cm) {
             return false;
